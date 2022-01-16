ed4aeba865e803f1e3ec3326b72b03fb302aae87
Mohammedameen3 <95864402+Mohammedameen3@users.noreply.github.com>
2022-01-16 18:10:23 +0530

    Add files via upload

---
+++ arraybeginnew.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class arraybeginnew
+{
+	public static void main(String args[])
+	{
+	  int i;
+	  Scanner sc=new Scanner(System.in);
+	  int[] num=new int[10];
+	  System.out.println( " Enter 10 numbers ");
+		for(i=0;i<10;i++)
+		 num[i]=sc.nextInt();
+		{
+		  
+		}
+		System.out.println(" 10 numbers are :");
+		for(i=0;i<10;i++)
+		{
+		System.out.print(num[i]+ "\t");
+		}
+		
+	}
+		  
+}
